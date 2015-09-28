bfce9d51c318d7d8ebdcaa472267669661284920
deletethis <mcdeletethis@gmail.com>
2015-09-28 15:03:01 -0700

    Added block protection to town plots

---
+++ src/main/java/deletethis/civilization/handler/BlockBreakEventHandler.java
@@ -0,0 +1,39 @@
+package deletethis.civilization.handler;
+
+import deletethis.civilization.Plot;
+import deletethis.civilization.Resident;
+import deletethis.civilization.Town;
+import deletethis.civilization.world.CivilizationWorldData;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class BlockBreakEventHandler
+{
+	@SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+	public void onBlockBreak(BlockEvent.BreakEvent event)
+	{
+		EntityPlayer player = event.getPlayer();
+		Resident resident = new Resident(player.getGameProfile().getId().toString());
+		World world = player.worldObj;
+		BlockPos pos = event.pos;
+		CivilizationWorldData data = CivilizationWorldData.get(world);
+		
+		if(!world.isRemote)
+		{
+			for(Town town : data.getTowns())
+			{
+				for(Plot plot : town.getPlots())
+				{
+					if(plot.isInPlot(world, pos) && !town.hasResident(resident))
+					{
+						event.setCanceled(true);
+					}
+				}
+			}
+		}
+	}
+}
--- src/main/java/deletethis/civilization/proxy/CommonProxy.java
+++ src/main/java/deletethis/civilization/proxy/CommonProxy.java
@@ -1,12 +1,15 @@
 package deletethis.civilization.proxy;
 
+import deletethis.civilization.handler.BlockBreakEventHandler;
 import deletethis.civilization.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
 
 public abstract class CommonProxy
 {
 	public void preInit()
 	{
 		ModItems.registerItems();
+		MinecraftForge.EVENT_BUS.register(new BlockBreakEventHandler());
 	}
 	
 	public void init()
